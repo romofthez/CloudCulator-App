248b32ef7b02c506488a5efff8d9cd549eb32706
OBJECTIVITY\kromaniuk <kromaniuk@objectivity.co.uk>
2023-04-09 23:40:55 +0200

    Add ICalculatorEngine interface

---
+++ CloudCulator/Cloudculator.Core/ICalculatorEngine.cs
@@ -0,0 +1,11 @@
+﻿namespace Cloudculator.Core
+{
+    internal interface ICalculatorEngine
+    {
+        double Add(double left, double right);
+        double Subtract(double left, double right);
+        double Multiply(double left, double right);
+        double Divide(double divergent, double divisor);
+
+    }
+}
