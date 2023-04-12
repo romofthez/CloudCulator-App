95bfc3d42d5759bb8dd10c83570601689b007918
OBJECTIVITY\kromaniuk <kromaniuk@objectivity.co.uk>
2023-04-12 20:09:25 +0200

    Created class which implements ICalculatorEngine

---
+++ CloudCulator/Cloudculator.Core/CalculatorEngine.cs
@@ -0,0 +1,27 @@
+﻿using Cloudculator.Core.Interfaces;
+
+namespace Cloudculator.Core
+{
+    public class CalculatorEngine : ICalculatorEngine
+    {
+        public double Add(double left, double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double divergent, double divisor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiply(double left, double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Subtract(double left, double right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
