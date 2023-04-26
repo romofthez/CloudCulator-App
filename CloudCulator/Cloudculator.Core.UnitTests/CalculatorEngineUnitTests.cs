9a6c475fe62842e69567cecf2e6c7e410da19e07
OBJECTIVITY\kromaniuk <kromaniuk@objectivity.co.uk>
2023-04-26 18:42:58 +0200

    Implemented add operatioon and created unit tests

---
+++ CloudCulator/Cloudculator.Core.UnitTests/CalculatorEngineUnitTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Cloudculator.Core.UnitTests
+{
+    public class CalculatorEngineUnitTests
+    {
+        [Fact]
+        public void Add_TwoNumbers_ReturnAddition()
+        {
+            //Arrange
+            var service = new CalculatorEngine();
+            double leftArgument = 5;
+            double rightArgument = 10.5;
+            double expectedResult = 15.5;
+
+            //Act
+            var result = service.Add(leftArgument, rightArgument);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Add_AdditionOfArgumentsOverflowsDoubleMaxPrecision_ThrowsOverflowException()
+        {
+            //Arrange
+            var service = new CalculatorEngine();
+            var leftArgument = double.MinValue;
+            var rightArgument = double.MinValue;
+
+            //Act
+            //Assert
+            Assert.Throws<OverflowException>(() => service.Add(leftArgument, rightArgument));
+        }
+    }
+}
--- CloudCulator/Cloudculator.Core/CalculatorEngine.cs
+++ CloudCulator/Cloudculator.Core/CalculatorEngine.cs
@@ -6,7 +6,13 @@ namespace Cloudculator.Core
     {
         public double Add(double left, double right)
         {
-            throw new NotImplementedException();
+            var result = left + right;
+            if(double.IsInfinity(result))
+            {
+                throw new OverflowException();
+            };
+
+            return result;
         }
 
         public double Divide(double divergent, double divisor)
