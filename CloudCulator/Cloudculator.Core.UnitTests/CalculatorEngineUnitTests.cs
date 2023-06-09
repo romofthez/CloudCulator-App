93dea81ce0ae801efcd33d58e744a10c47967d18
OBJECTIVITY\kromaniuk <kromaniuk@objectivity.co.uk>
2023-06-09 08:46:23 +0200

    Resolved remaining PR comments

--- CloudCulator/Cloudculator.Core.UnitTests/CalculatorEngineUnitTests.cs
+++ CloudCulator/Cloudculator.Core.UnitTests/CalculatorEngineUnitTests.cs
@@ -6,7 +6,7 @@ namespace Cloudculator.Core.UnitTests
     {
         private CalculatorEngine service;
 
-        CalculatorEngineUnitTests()
+        public CalculatorEngineUnitTests()
         {
             service = new CalculatorEngine();
         }
@@ -27,7 +27,7 @@ namespace Cloudculator.Core.UnitTests
         }
 
         [Fact]
-        public void Add_AdditionOfArgumentsOverflowsDoubleMaxPrecision_ThrowsOverflowException()
+        public void Add_BothArgumentsAreMinValues_ThrowsOverflowException()
         {
             //Arrange
             var leftArgument = double.MinValue;
@@ -36,6 +36,99 @@ namespace Cloudculator.Core.UnitTests
             //Act
             //Assert
             Assert.Throws<OverflowException>(() => service.Add(leftArgument, rightArgument));
+        }
+
+        [Fact]
+        public void Add_BothArgumentsAreMaxValues_ThrowsOverflowException()
+        {
+            //Arrange
+            var leftArgument = double.MaxValue;
+            var rightArgument = double.MaxValue;
+
+            //Act
+            //Assert
+            Assert.Throws<OverflowException>(() => service.Add(leftArgument, rightArgument));
+        }
+
+        [Fact]
+        public void Add_LeftArgumentIsMaxValueRightArgumentIsMinValue_ThrowsOverflowException()
+        {
+            //Arrange
+            var leftArgument = double.MaxValue;
+            var rightArgument = double.MinValue;
+
+            //Act
+            //Assert
+            Assert.Throws<OverflowException>(() => service.Add(leftArgument, rightArgument));
+        }
+
+        [Fact]
+        public void Add_BothArgumentsArePositiveInfinities_ThrowsOverflowException()
+        {
+            //Arrange
+            var leftArgument = double.PositiveInfinity;
+            var rightArgument = double.PositiveInfinity;
+
+            //Act
+            //Assert
+            Assert.Throws<OverflowException>(() => service.Add(leftArgument, rightArgument));
+        }
+
+        [Fact]
+        public void Add_BothArgumentsAreNegativeInfinities_ThrowsOverflowException()
+        {
+            //Arrange
+            var leftArgument = double.PositiveInfinity;
+            var rightArgument = double.PositiveInfinity;
+
+            //Act
+            //Assert
+            Assert.Throws<OverflowException>(() => service.Add(leftArgument, rightArgument));
+        }
+
+        [Fact]
+        public void Add_BothArgumentsAreNaNs_ReturnsNaN()
+        {
+            //Arrange
+            var leftArgument = double.NaN;
+            var rightArgument = double.NaN;
+            var expected = double.NaN;
+
+            //Act
+            var result = service.Add(leftArgument, rightArgument);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Add_LeftArgumentsIsNaNRightArgumentIsPositiveInfinity_ReturnsNaN()
+        {
+            //Arrange
+            var leftArgument = double.NaN;
+            var rightArgument = double.PositiveInfinity;
+            var expected = double.NaN;
+
+            //Act
+            var result = service.Add(leftArgument, rightArgument);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Add_LeftArgumentsIsPositiveInfinityArgumentIsNegativeInfinity_ReturnsNaN()
+        {
+            //Arrange
+            var leftArgument = double.PositiveInfinity;
+            var rightArgument = double.NegativeInfinity;
+            var expected = double.NaN;
+
+            //Act
+            var result = service.Add(leftArgument, rightArgument);
+
+            //Assert
+            Assert.Equal(expected, result);
         }
     }
 }
