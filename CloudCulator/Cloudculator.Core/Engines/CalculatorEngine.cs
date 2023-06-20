3bbe58fd6f9ef1feaa0705f5faee7798ef285e7c
romofthez <58038371+romofthez@users.noreply.github.com>
2023-06-20 23:36:13 +0200

    Phase1/feature/tb1 003 implement add operation 1 (#3)
    * Implemented naming suggestions to projects
    
    * Add ICalculatorEngine interface
    
    * Implement PR suggestions
    
    * Delete unnecessary folders
    
    * Created class which implements ICalculatorEngine
    
    * Implemented add operatioon and created unit tests
    
    * Implemented PR suggestions
    
    * Resolved remaining PR comments
    
    * Resolved final comment
    
    ---------
    
    Co-authored-by: OBJECTIVITY\kromaniuk <kromaniuk@objectivity.co.uk>

---
+++ CloudCulator/Cloudculator.Core/Engines/CalculatorEngine.cs
@@ -0,0 +1,33 @@
+﻿using Cloudculator.Core.Interfaces;
+
+namespace Cloudculator.Core.Engines
+{
+    public class CalculatorEngine : ICalculatorEngine
+    {
+        public double Add(double left, double right)
+        {
+            var result = left + right;
+            if (double.IsInfinity(result))
+            {
+                throw new OverflowException();
+            };
+
+            return result;
+        }
+
+        public double Divide(double divergent, double divisor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiply(double left, double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Subtract(double left, double right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CloudCulator/Cloudculator.Core/Interfaces/ICalculatorEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace Cloudculator.Core.Interfaces
+{
+    public interface ICalculatorEngine
+    {
+        double Add(double left, double right);
+        double Subtract(double left, double right);
+        double Multiply(double left, double right);
+        double Divide(double divergent, double divisor);
+    }
+}
